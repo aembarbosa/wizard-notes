bff10bf0aaf47c1864506f93b044a5b5417d7a33
Alyne Emmily Menezes Barbosa <aembarbosa@outlook.com>
2023-11-15 21:06:19 -0300

    subindo projeto rest inicial em spring boot

---
+++ wizard_notes_rest_api/src/main/java/aem/barbosa/wizard_notes/WizardNotesApplication.java
@@ -0,0 +1,13 @@
+package aem.barbosa.wizard_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizardNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WizardNotesApplication.class, args);
+	}
+
+}
---
+++ wizard_notes_rest_api/src/test/java/aem/barbosa/wizard_notes/WizardNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package aem.barbosa.wizard_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizardNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
